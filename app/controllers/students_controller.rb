2b57f674065afbd0581ac4183e24d6f7bdbf5506
Chun Yi <8.5by11@gmail.com>
2022-03-14 13:37:50 -0400

    commit tree 4a0d468f70b907c8779526ad944910511fff6674

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+
+    # students = Student.find_by(first_name: params[:first_name])
+    # render json: students
+
+    # student = Student.where(first_name: params[:first_name], last_name: params[:last_name])
+    student = Student.find_by(first_name: params[:first_name], last_name: params[:last_name])
+    render json: student
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
   end
 
 end
